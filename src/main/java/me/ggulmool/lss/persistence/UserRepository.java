42ee4a55786e7a2c616588c8247ad4876c41ef4e
ggulmool <kimny86@gmail.com>
2018-02-14 15:09:42 +0900

    step1-1 : step1 project create

---
+++ src/main/java/me/ggulmool/lss/persistence/UserRepository.java
@@ -0,0 +1,15 @@
+package me.ggulmool.lss.persistence;
+
+import me.ggulmool.lss.web.model.User;
+
+public interface UserRepository {
+
+    Iterable<User> findAll();
+
+    User save(User user);
+
+    User findUser(Long id);
+
+    void deleteUser(Long id);
+
+}
---
+++ src/main/java/me/ggulmool/lss/spring/LssApp.java
@@ -0,0 +1,35 @@
+package me.ggulmool.lss.spring;
+
+import me.ggulmool.lss.persistence.InMemoryUserRepository;
+import me.ggulmool.lss.persistence.UserRepository;
+import me.ggulmool.lss.web.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.convert.converter.Converter;
+
+@SpringBootApplication
+@ComponentScan("me.ggulmool.lss.web")
+public class LssApp {
+
+    @Bean
+    public UserRepository userRepository() {
+        return new InMemoryUserRepository();
+    }
+
+    @Bean
+    public Converter<String, User> messageConverter() {
+        return new Converter<String, User>() {
+            @Override
+            public User convert(String id) {
+                return userRepository().findUser(Long.valueOf(id));
+            }
+        };
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(LssApp.class, args);
+    }
+
+}
