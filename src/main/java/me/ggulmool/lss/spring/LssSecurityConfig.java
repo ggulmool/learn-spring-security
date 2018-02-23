a1048737c1e3a2d0c412a58c1f0e12e74517f8f1
ggulmool <kimny86@gmail.com>
2018-02-23 14:21:55 +0900

    step5-1 : By URL Authorization with Expressions

---
+++ src/main/java/me/ggulmool/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,47 @@
+package me.ggulmool.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+            inMemoryAuthentication()
+            .withUser("user").password("pass").roles("USER").and()
+            .withUser("admin").password("pass").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            
+            // .antMatchers("/secured").access("hasRole('USER')")
+        
+            .anyRequest().authenticated()
+        
+        .and()
+        .formLogin().
+            loginPage("/login").permitAll().
+            loginProcessingUrl("/doLogin")
+
+        .and()
+        .logout().permitAll().logoutUrl("/logout")
+        
+        .and()
+        .csrf().disable();
+    }
+
+}
